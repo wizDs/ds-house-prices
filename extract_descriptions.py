f0a19aeebad5dceaa824b11be01a6e13113ebfab
wiz <wilsonzhou92@gmail.com>
2023-12-31 11:07:59 +0100

    to sklearn

--- extract_descriptions.py
+++ extract_descriptions.py
@@ -1,31 +1,56 @@
 import re
 import pathlib
+from typing import Optional
+import pandas as pd
+import pandas_profiling
+import streamlit as st
 from functools import partial
 from pydantic import BaseModel
-from typing import List
-
-curr_path = pathlib.Path('.')
+from streamlit_pandas_profiling import st_profile_report
 
 class FeatureDescription(BaseModel):
     name: str
     desc: str
 
-class FeatureDescriptions(BaseModel):
-    features: List[FeatureDescription] 
+def read_feature_descriptions(path: pathlib.Path) -> pd.DataFrame:
+    # read feature names from description file
+    with open(path) as f:
+        features = f.readlines()
+        features = filter(lambda s: ":" in s, features)
+        features = filter(partial(re.match, "\\w"), features)
+        features = map(partial(re.sub, '\\n', ''), features)
+        features = map(partial(re.sub, '\\t', ''), features)
+        features = map(partial(re.split, ':\\s+?'), features)
+        features = map(lambda x: FeatureDescription(name=x[0], desc=x[1]), features)
+        features = pd.DataFrame(x.dict() for x in features)
+        
+    return features
 
+def generate_streamlit_app(dataframes: list[pd.DataFrame], dataraport: Optional[pd.DataFrame] = None) -> None:
+    for df in dataframes:
+        st.table(df)
 
+    if dataraport is not None:
+        st_profile_report(dataraport.profile_report())
 
-# read feature names from description file
-with open(curr_path / 'data' / 'data_description.txt') as f:
-    features = f.readlines()
-    features = filter(lambda s: ":" in s, features)
-    features = filter(partial(re.match, "\\w"), features)
-    features = map(partial(re.sub, '\\n', ''), features)
-    features = map(partial(re.sub, '\\t', ''), features)
-    features = map(partial(re.split, ':\\s+?'), features)
-    features = map(lambda x: FeatureDescription(name=x[0], desc=x[1]), features)
-    features = FeatureDescriptions(features=list(features))
-
-
-with open(curr_path / 'data' / 'feature_description.txt', 'w') as f:
-    f.write(features.json())
+if __name__ == '__main__':
+    
+    curr_path = pathlib.Path('.')   
+    feature_description = read_feature_descriptions(curr_path / 'data' / 'data_description.txt')
+    train = pd.read_csv(curr_path / "data" / "train.csv")
+    datatypes = train.dtypes.rename("datatype").rename_axis("name")
+    data_summarization = train.describe(include='all').transpose()[['mean', 'std', 'min', 'max', 'unique', 'top', 'freq']]
+    feature_description = feature_description \
+        .join(
+            datatypes, 
+            on='name'
+        ) \
+        .join(
+            data_summarization, 
+            on='name'
+        ) \
+        .round(decimals=2)
+    generate_streamlit_app(
+        dataframes=[feature_description, train.sample(5)],
+        dataraport=train
+    )
--- ml_model/model.py
+++ ml_model/model.py
@@ -9,40 +9,26 @@ import mlflow
 from toolz.itertoolz import pluck
 from sklearn.compose import make_column_selector, TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
-from sklearndf.pipeline import PipelineDF
-from sklearndf.transformation import StandardScalerDF, SimpleImputerDF
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
 from ml_model.eval_regression import ModelReport, ModelReportBuilder
 
 
-num_ppl = PipelineDF(
-    steps=[
-        ('scaler', StandardScalerDF()),
-        ('imputer', SimpleImputerDF(fill_value=-1)),
-        # TODO: Outlier detection
-    ]
-)
 
-model = Pipeline(
-    steps=[
-        ('preprocessor', num_ppl),
-        ('model', LinearRegression(n_jobs=-1)
-        
-        
-        )
-    ]
-)
+num_ppl = Pipeline(steps=[('scaler', StandardScaler().set_output(transform="pandas")),
+                          ('imputer', SimpleImputer(fill_value=-1).set_output(transform="pandas")),])
+                          # TODO: Outlier detection
+
+model = Pipeline(steps=[('preprocessor', num_ppl),
+                        ('model', LinearRegression(n_jobs=-1))])
 
 tt = TransformedTargetRegressor(regressor=LinearRegression(n_jobs=-1),
                                  func=np.log, inverse_func=np.exp)
 
-model_log = Pipeline(
-    steps=[
-        ('preprocessor', num_ppl),
-        ('model', tt)
-    ]
-)
+model_log = Pipeline(steps=[('preprocessor', num_ppl),
+                            ('model', tt)])
 
 
 if __name__ == '__main__':
@@ -55,12 +41,12 @@ if __name__ == '__main__':
     data_df.set_index('Id', inplace=True)
 
     # read feature descriptions
-    with open(curr_path / 'data' / 'feature_description.txt', 'r') as f:
+    with open(curr_path / 'data' / 'feature_description.txt', 'r', encoding='utf-8') as f:
         feature_descriptions = json.loads(f.read())['features']
 
     # mapper from name to description
     description_mapper = dict(pluck(['name','desc'], feature_descriptions))
-    
+
     num_cols = make_column_selector(dtype_include=np.number)
     bool_cols = make_column_selector(dtype_include=bool)
 
@@ -69,19 +55,19 @@ if __name__ == '__main__':
 
     kfold = KFold(random_state=423, shuffle=True)
     train_index, test_index = next(kfold.split(X[num_cols]))
-    
+
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
     model_log.fit(X_train[num_cols], y_train)
     y_pred = model_log.predict(X_test[num_cols])
-    
+
     model_report = ModelReportBuilder(X[num_cols], y / 1_000_000, model, kfold)
     model_report.summary.dict()
-    
+
     model_report = ModelReportBuilder(X[num_cols], y / 1_000_000, model_log, kfold)
     model_report.summary.dict()
-    
+
     # test data
     test_path = curr_path / 'data' / 'test.csv'
     test_df = pd.read_csv(test_path)
